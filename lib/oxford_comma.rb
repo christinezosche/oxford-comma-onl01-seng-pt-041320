041edab84e114d059b6f4643337ff843a7e6a7e4
Christine Zosche <christine.zosche@gmail.com>
2020-04-22 00:47:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-  array.join(", ")
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    array[-1].insert(0, "and ")
+    array.join(", ")
  end
